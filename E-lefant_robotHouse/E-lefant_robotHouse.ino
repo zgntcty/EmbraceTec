ed0d2c3274e0d48de928fc4af4e8ec8c5c9c94ea
Chen,Tianyi T <zgntcty@gmail.com>
2023-09-18 21:00:17 +0200

    Update E-lefant_robotHouse.ino

--- E-lefant_robotHouse/E-lefant_robotHouse.ino
+++ E-lefant_robotHouse/E-lefant_robotHouse.ino
@@ -3,14 +3,18 @@
 #define BPin 5
 #define servoPin 11
 const byte interruptPin = 2;//for all buttons
+const int buzzer = 10;
 volatile byte state = LOW;
 int functionNumber = 1;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), buttonPressed, CHANGE);
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(1000);        // ...for 1 sec
 }
 
 void loop() {
