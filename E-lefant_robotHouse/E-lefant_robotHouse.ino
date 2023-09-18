ef24bdb4715bdd3a9b16a0bb80ff6f62109fb0e0
Chen,Tianyi T <zgntcty@gmail.com>
2023-09-18 20:51:37 +0200

    Initial commit
    robot code and schematic diagram

---
+++ E-lefant_robotHouse/E-lefant_robotHouse.ino
@@ -0,0 +1,36 @@
+#define RPin 9
+#define GPin 6
+#define BPin 5
+#define servoPin 11
+const byte interruptPin = 2;//for all buttons
+volatile byte state = LOW;
+int functionNumber = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), buttonPressed, CHANGE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+switch (var) {
+  case 1:
+    //When button not pressed
+    break;
+  case 2:
+    //When button pressed.
+    functionNumber = 1;
+    break;
+  default:
+    // if nothing else matches, do the default
+    // default is optional
+    Serial.println("Unknown state");
+    break;
+}
+}
+
+void buttonPressed() {
+    functionNumber = 2;
+}
